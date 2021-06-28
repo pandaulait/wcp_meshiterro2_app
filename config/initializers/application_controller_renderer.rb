93964dd8e9a66e18da49dfaa698ce9995f3fe22d
pandaulait <show.hey.panda@gmail.com>
2021-06-28 09:23:23 +0000

    [Update]いいね機能,コメント機能,ページング機能

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '01fc1624ec62fa3280fe3db1114e32078bcd3ec7e296c23f9221b1a0850351e98f03cb523d96a2857bf44d48c0a797d1c08d4a4a94edc8ca5ff81e28bba58781'
